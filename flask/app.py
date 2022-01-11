8bc1993d5fd41dc32cdc7a7c3f3498ed5448cbd0
ugbd <ekarry@yandex.ru>
2022-01-11 21:58:57 +0300

    google

--- flask/app.py
+++ flask/app.py
@@ -14,7 +14,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
-UPLOAD_FOLDER = os.getenv("DIR", "./flies ")
+UPLOAD_FOLDER = os.getenv("DIR", "/home")
 USER = os.getenv("USER", "Usver")
 TOKEN = os.getenv("TOKEN", "blabla")
 
